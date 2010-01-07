93b1198b48b482ab7b6a9b770005e03c3d90c83d
Yuya Kato <yuyakato@gmail.com>
2010-01-07 15:39:42 +0900

    分類結果を登録するスクリプトを追加

---
+++ mlp1/40_tagging.rb
@@ -0,0 +1,31 @@
+#! ruby -Ku
+
+require "cgi"
+require "open-uri"
+require "rubygems"
+require "wsse"
+
+def read_credential
+  File.open("config/ironnews.id") { |file|
+    return [file.gets.chomp, file.gets.chomp]
+  }
+end
+
+def create_token(username, password)
+  return Wsse::UsernameToken.build(username, password).format
+end
+
+def tagging(article_id, tag)
+  username, password = read_credential
+  token = create_token(username, password)
+  url  = "http://ironnews.nayutaya.jp/api/add_tags?article_id=#{article_id}&tag1=#{CGI.escape(tag)}"
+  json = open(url, {"X-WSSE" => token}) { |io| io.read }
+end
+
+File.foreach("tagging.out") { |line|
+  tag, article_id, title = line.chomp.split(/\t/)
+  puts(article_id)
+  tagging(article_id, tag)
+
+  sleep(0.1)
+}
