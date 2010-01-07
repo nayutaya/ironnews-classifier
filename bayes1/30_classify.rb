2860d0219c38105ca6f1317a3f69f8f07f144c17
Yuya Kato <yuyakato@gmail.com>
2010-01-07 13:57:01 +0900

    分類用のスクリプトを追加

---
+++ bayes1/30_classify.rb
@@ -0,0 +1,32 @@
+#! ruby -Ku -Ilib
+
+# 単純ベイズ分類器によるカテゴリ分類を行う
+
+require "naive_bayes_categorizer"
+
+DB_FILENAME = "db.out"
+
+STDERR.puts("loading...")
+tokenizer   = BigramTokenizer.new
+categorizer = NaiveBayesCategorizer.load(tokenizer, DB_FILENAME)
+
+thresholds = {
+  "rail" => 1.0,
+  "rest" => 3.5,
+}
+
+out = File.open("tagging.out", "wb")
+
+STDERR.puts("categorizing...")
+File.foreach("untagged_articles.out") { |line|
+  article_id, title = line.chomp.split(/\t/)
+  category = categorizer.categorize(title, thresholds)
+  category_ja =
+    case category
+    when "rail" then "鉄道"
+    when "rest" then "非鉄"
+    when nil    then "不明"
+    else raise
+    end
+  out.puts([category_ja, article_id, title].join("\t"))
+}
