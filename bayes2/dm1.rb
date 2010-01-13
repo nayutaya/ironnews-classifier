71a6cae34d05593d2a7b5922715f1db51d44f0a4
Yuya Kato <yuyakato@gmail.com>
2010-01-13 11:57:41 +0900

    関連を追加

--- bayes2/dm1.rb
+++ bayes2/dm1.rb
@@ -4,19 +4,30 @@ require "rubygems"
 require "dm-core"
 require "facets"
 
-DataMapper.setup(:default, "sqlite3:#{File.expand_path(__DIR__)}/test.db")
+DataMapper.setup(:default, "sqlite3:#{File.expand_path(__DIR__)}/brain.out")
 
 class Document
   include DataMapper::Resource
 
   property :id,   Serial
-  property :body, String
+  property :body, Text, :required => true, :lazy => false
+
+  has n, :features
+end
+
+class Feature
+  include DataMapper::Resource
+
+  property :id,      Serial
+  property :feature, Text, :required => true, :lazy => false
+
+  belongs_to :document
 end
 
 DataMapper.auto_migrate!
 
-=begin
-class Feature
-  include DataMapper::Resource
-end
-=end
+f1 = Feature.new(:feature => "f1")
+
+d1 = Document.new(:body => "d1")
+d1.features << f1
+d1.save
