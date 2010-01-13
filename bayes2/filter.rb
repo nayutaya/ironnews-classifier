ff4de011b4713e1daae0f379a946af38c7415ed0
Yuya Kato <yuyakato@gmail.com>
2010-01-13 16:09:42 +0900

    フィルタを追加

--- bayes2/filter.rb
+++ bayes2/filter.rb
@@ -5,23 +5,35 @@ require "nkf"
 File.open("out.txt", "wb") { |file|
   File.foreach("in.txt") { |line|
     line.chomp
-#line.gsub!(/\xE2\x80\x94/, "-")  # EM DASH
-#line.gsub!(/\xE3\x80\x9C/, "～") # WAVE DASH
-#line = NKF.nkf("-W -w80 -m0 -Z1", line)
 
-#line.gsub!(/([^A-Za-z])([A-Za-z])/, '\1 \2') # 非アルファベット-アルファベット境界にスペースを挿入
-#line.gsub!(/([A-Za-z])([^A-Za-z])/, '\1 \2') # アルファベット-非アルファベット境界にスペースを挿入
+    line.gsub!(/\xE2\x80\x94/, "-")  # EM DASH
+    line.gsub!(/\xE3\x80\x9C/, "～") # WAVE DASH
 
-line.gsub!(/([^ァ-ヺー])([ァ-ヺー])/, '\1 \2') # 非カタカナ-カタカナ境界にスペースを挿入
-line.gsub!(/([ァ-ヺー])([^ァ-ヺー])/, '\1 \2') # 非カタカナ-カタカナ境界にスペースを挿入
+    line = NKF.nkf("-W -w80 -m0 -Z1", line)
+    line.downcase!
 
-#line.gsub!(/[ぁ-ゞ]+/, " ")
-#line.gsub!(/[、。]/, " ")
-#line.gsub!(/[「」]/, " ")
-#line.gsub!(/[【】]/, " ")
+    line.gsub!(/([^a-z])([a-z])/, '\1 \2') # 非アルファベット-アルファベット境界にスペースを挿入
+    line.gsub!(/([a-z])([^a-z])/, '\1 \2') # アルファベット-非アルファベット境界にスペースを挿入
+    line.gsub!(/([^ァ-ヺー0-9])([ァ-ヺー])/, '\1 \2') # 非カタカナ-カタカナ境界にスペースを挿入
+    line.gsub!(/([ァ-ヺー])([^ァ-ヺー])/, '\1 \2')    # 非カタカナ-カタカナ境界にスペースを挿入
+    line.gsub!(/([^0-9])([0-9])/, '\1 \2') # 非数字-数字境界にスペースを挿入
 
-line.gsub!(/ +/, " ")
-line.strip!
+    line.gsub!(/[ぁ-ゞ]+/, " ")
+    line.gsub!(/[、。]/, " ")
+    line.gsub!(/[・‥…]/, " ")
+    line.gsub!(/[～]/, " ")
+    line.gsub!(/[「」]/, " ")
+    line.gsub!(/[【】]/, " ")
+    line.gsub!(/[\x21-\x23]/, " ")
+    line.gsub!(/[\x26-\x2F]/, " ")
+    line.gsub!(/[\x3A-\x40]/, " ")
+    line.gsub!(/[\x5C-\x60]/, " ")
+    line.gsub!(/[\x7B-\x7E]/, " ")
+
+    line.gsub!(/ +/, " ")
+    line.strip!
+    line = line.split(/ /).select { |str| str.scan(/./).size >= 2 }.join(" ")
+
     file.puts(line)
   }
 }
