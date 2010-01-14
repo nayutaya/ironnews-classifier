674793cfe1b140325752f17dfe756517efbf1ccd
Yuya Kato <yuyakato@gmail.com>
2010-01-14 16:32:20 +0900

    インデックスを追加

--- bayes2/lib/models/feature.rb
+++ bayes2/lib/models/feature.rb
@@ -3,7 +3,7 @@ class Feature
   include DataMapper::Resource
 
   property :id,      Serial
-  property :feature, String, :required => true, :length => 20
+  property :feature, String, :required => true, :length => 20, :index => true
 
   belongs_to :document
 end
