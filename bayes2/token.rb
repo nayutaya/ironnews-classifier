4f4f9e9d0fc550f68f65569a367099170abe8df2
Yuya Kato <yuyakato@gmail.com>
2010-01-14 11:23:13 +0900

    2-gramの統計情報を出力する実験コードを追加

--- bayes2/token.rb
+++ bayes2/token.rb
@@ -4,8 +4,20 @@ require "bigram_tokenizer"
 
 tokenizer = BigramTokenizer.new
 
+table = Hash.new(0)
+
 titles = File.foreach("../../ironnews-data/news_title/rail_01.txt").map { |line| line.strip }
 titles.each { |title|
+  puts
   puts(title)
+  tokens = tokenizer.tokenize(title)
   p(tokenizer.tokenize(title))
+  tokens.each { |token|
+    table[token] += 1
+  }
+}
+
+puts
+table.to_a.sort_by { |k, v| -v }.each { |token, count|
+  p([token, count])
 }
