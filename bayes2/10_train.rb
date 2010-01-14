cb4cc9961c8107a686fc28e4f577f34768d84509
Yuya Kato <yuyakato@gmail.com>
2010-01-14 14:28:54 +0900

    リファクタリング

--- bayes2/10_train.rb
+++ bayes2/10_train.rb
@@ -6,30 +6,29 @@ require "bigram_tokenizer"
 STDERR.puts("initialize...")
 tokenizer = BigramTokenizer.new
 
+STDERR.puts("cleaning...")
+Document.all.destroy!
+Feature.all.destroy!
+
 STDERR.puts("training...")
-transaction = DataMapper::Transaction.new(Document, Feature)
-transaction.commit {
-  [
-    ["鉄道", "../../ironnews-data/news_title/rail_01.txt"],
-#    ["鉄道", "../../ironnews-data/news_title/rail_*.txt"],
-#    ["非鉄", "../../ironnews-data/news_title/rest_*.txt"],
-  ].each { |category, pattern|
-    Dir.glob(pattern).each { |path|
-      STDERR.puts("#{category} #{path}")
-      File.foreach(path) { |line|
-        title = line.chomp
+[
+#  ["鉄道", "rail_xx.txt"],
+#  ["鉄道", "../../ironnews-data/news_title/rail_xx.txt"],
+  ["鉄道", "../../ironnews-data/news_title/rail_*.txt"],
+  ["非鉄", "../../ironnews-data/news_title/rest_*.txt"],
+].each { |category, pattern|
+  Dir.glob(pattern).each { |path|
+    STDERR.puts("#{category} #{path}")
+    File.foreach(path) { |line|
+      title  = line.chomp
+      tokens = tokenizer.tokenize(title)
 
-        document = Document.new(:body => title, :category => category)
+      document = Document.new(:body => title, :category => category)
+      document.features = tokens.map { |token| Feature.new(:feature => token) }
+      document.save
 
-        tokens = tokenizer.tokenize(title)
-        tokens.each { |token|
-          document.features << Feature.new(:feature => token)
-        }
-
-        document.save
-        STDERR.write(".")
-      }
-      STDERR.puts
+      STDERR.write(".")
     }
+    STDERR.puts
   }
 }
--- bayes2/config.rb
+++ bayes2/config.rb
@@ -7,20 +7,17 @@ require "facets"
 require "models/document"
 require "models/feature"
 
-DB_FILENAME = "brain.out"
-
 #DataMapper::Logger.new(STDOUT, :debug)
 
 case :postgres
 when :sqlite3
-  DataMapper.setup(:default, "sqlite3:#{__DIR__}/#{DB_FILENAME}")
+  DataMapper.setup(:default, "sqlite3:#{__DIR__}/brain.out")
 when :postgres
-  DataMapper.setup(:default,
+  DataMapper.setup(
+    :default,
     :adapter  => "postgres",
     :database => "bayes2",
     :username => "postgres",
     :password => nil)
 else raise
 end
-
-DataMapper.auto_migrate!
