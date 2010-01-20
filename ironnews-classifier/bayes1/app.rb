c0b4dff0712ee7ea6bee46508e5d13ecd7cde01f
Yuya Kato <yuyakato@gmail.com>
2010-01-20 11:47:11 +0900

    学習処理を実装

--- ironnews-classifier/bayes1/app.rb
+++ ironnews-classifier/bayes1/app.rb
@@ -1,5 +1,6 @@
 
 require "bayes1/models"
+require "bayes1/tokenizer"
 
 get "/bayes1" do
   "bayes1"
@@ -40,12 +41,36 @@ get "/bayes1/remove" do
 end
 
 get "/bayes1/train" do
-=begin
-  query = AppEngine::Datastore::Query.new("BayesOneDocument")
-  query.filter(:trained, "=", false)
-  query.java_query.setKeysOnly
+  tokenizer = BayesOneTokenizer.new
 
-  documents = query.fetch(:limit => 100).to_a
-=end
-  erb(:test)
+  all_documents = BayesOneDocument.all(
+    :trained => false,
+    :limit   => 100)
+
+  target_documents = all_documents.
+    sort_by { rand }.
+    slice(0, 3)
+
+  target_documents.each { |document|
+    # カテゴリの文書数をインクリメント
+    category = BayesOneCategory.find_or_create(:name => document.category)
+    category.quantity += 1
+    category.save
+
+    # 特徴の特徴数をインクリメント
+    tokens = tokenizer.tokenize(document.body)
+    tokens.each { |token|
+      feature = BayesOneFeature.find_or_create(
+        :category => document.category,
+        :feature  => token)
+      feature.quantity += 1
+      feature.save
+    }
+
+    # 学習済みに変更
+    document.trained = true
+    document.save
+  }
+
+  ""
 end
