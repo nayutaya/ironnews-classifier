448b9f863fdd17ec09700a808dd0fbb9499797f4
Yuya Kato <yuyakato@gmail.com>
2010-01-21 14:39:42 +0900

    分類テスト用のスクリプトを修正

--- ironnews-classifier/bayes1_classify.rb
+++ ironnews-classifier/bayes1_classify.rb
@@ -8,7 +8,13 @@ require "json"
 HOST = "localhost:8080"
 #HOST = "ironnews-classifier1.appspot.com"
 
-url  = "http://#{HOST}/bayes1/classify"
-url += "?body=" + CGI.escape("「ＪＲ西歴代３社長起訴を」　脱線事故遺族、検察審に申し立て")
+body = "「ＪＲ西歴代３社長起訴を」　脱線事故遺族、検察審に申し立て"
+puts(body)
 
-open(url) { |io| p(JSON.parse(io.read)) }
+url  = "http://#{HOST}/bayes1/classify"
+url += "?body=" + CGI.escape(body)
+
+result = open(url) { |io| JSON.parse(io.read) }
+result.each { |category, prob|
+  puts("#{category}: #{prob}")
+}
