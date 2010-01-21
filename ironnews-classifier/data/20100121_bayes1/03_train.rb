3611bfcdc6e087c498e8a256931d2dc5e49eceeb
Yuya Kato <yuyakato@gmail.com>
2010-01-21 13:35:55 +0900

    学習を行うためのスクリプトを追加

---
+++ ironnews-classifier/data/20100121_bayes1/03_train.rb
@@ -0,0 +1,16 @@
+#! ruby -Ku
+
+# Google App Engine上の単純ベイズ分類器で学習を行う
+
+require "uri"
+require "net/http"
+
+uri = URI.parse("http://ironnews-classifier1.appspot.com/bayes1/train")
+
+Net::HTTP.start(uri.host, uri.port) { |http|
+  loop {
+    response = http.get(uri.path)
+    p(response.code)
+    sleep(30)
+  }
+}
