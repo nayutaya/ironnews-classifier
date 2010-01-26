19db033a14493ca78695f0c24f9b5dbaa26b0805
Yuya Kato <yuyakato@gmail.com>
2010-01-26 11:38:30 +0900

    ironnews-classifier1を使って分類を行うスクリプトを追加

---
+++ assessment/20_classify.rb
@@ -0,0 +1,44 @@
+#! ruby -Ku
+
+# ironnews-classifier1を使って記事を分類する
+
+require "cgi"
+require "open-uri"
+require "rubygems"
+require "json"
+
+require "config"
+
+THRESHOLDS = {
+  "鉄道" => 1.0,
+  "非鉄" => 3.5,
+}
+
+def classify(body)
+  url  = "http://ironnews-classifier1.appspot.com/bayes1/classify"
+  url += "?body=" + CGI.escape(body)
+  json = open(url) { |io| io.read }
+  return JSON.parse(json)
+end
+
+def categorize(body)
+  probs = classify(body).
+    map     { |category, prob| [category, prob] }.
+    sort_by { |category, prob| prob }
+
+  first_category,  first_prob  = probs[-1]
+  second_category, second_prob = probs[-2]
+
+  if first_prob > second_prob * (THRESHOLDS[first_category] || 1.0)
+    return first_category
+  else
+    return nil
+  end
+end
+
+articles = Article.all(:gae_bayes1 => nil, :order => [:article_id.asc])
+articles.each { |article|
+  p article
+  article.gae_bayes1 = categorize(article.title) || "不明"
+  article.save!
+}
