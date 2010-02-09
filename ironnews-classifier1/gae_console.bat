f6d64c765887de11bdf04b9e2ab8358526a53a8b
Yuya Kato <yuyakato@gmail.com>
2010-02-09 23:33:57 +0900

    マシン別の設定を追加

--- ironnews-classifier1/gae_console.bat
+++ ironnews-classifier1/gae_console.bat
@@ -1,3 +1,23 @@
-@set PATH=%PATH%;C:\Program Files\Java\jdk1.6.0_13\bin
-@set RUBYOPT=-rrubygems
-@cmd
+@echo off
+
+if "%COMPUTERNAME%" == "MACBETH" goto MACBETH
+if "%COMPUTERNAME%" == "CAESAR2" goto CAESAR2
+goto ELSE
+
+:MACBETH
+set PATH=%PATH%;C:\Program Files\Java\jdk1.6.0_13\bin
+set RUBYOPT=-rrubygems
+goto END
+
+:CAESAR2
+set PATH=%PATH%;C:\Program Files\Java\jdk1.6.0_17\bin
+set RUBYOPT=-rrubygems
+goto END
+
+:ELSE
+echo Unknown Host
+goto END
+
+:END
+
+cmd
